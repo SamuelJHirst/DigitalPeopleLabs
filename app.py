362bb71a0ff03bef478fb2b5c1108168c8e6f073
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2022-02-19 15:21:12 +0000

    Create a navigation bar

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", route="/")
 
 app.run("127.0.0.1", 8080, debug=True)
